ea735930bf417338cc6caf0cee861549f11fc178
Gustavo De Almeida Bassanetto <gustavo.almeidabassanetto@gmail.com>
2023-03-13 20:46:52 -0300

    C# Herança e Interfaces :: Implementação da classe Funcionario

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using bytebank_adm.Funcionarios;
+
+Funcionario pedro = new Funcionario();
+pedro.Nome = "Pedro Malazartes";
+pedro.Cpf = "123456789";
+pedro.Salario = 2000;
+
+Console.WriteLine(pedro.Nome);
+Console.WriteLine(pedro.GetBonificacao());
